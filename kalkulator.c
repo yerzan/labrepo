684697e65d766aecc04ae25d4d0212e58445013c
MichalJanikUR <wmicjan86@gmail.com>
2025-01-13 10:44:32 +0100

    Dodanie funkcji dzielenia a i b

--- kalkulator.c
+++ kalkulator.c
@@ -1,7 +1,17 @@
 #include <stdio.h>
-int main() {
 
+double dzielenie(double a, double b)
+{
+    if (b != 0)
+    {
+        return a / b;
+    }
+    else exit(0);
+}
 
-    
-return 0;
-} 
+int main()
+{
+    printf("%f",dzielenie(3,4));
+    printf("\n%f",dzielenie(3,0));
+    return 0;
+}
