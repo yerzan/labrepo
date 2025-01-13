19a6c3c91d79a96ce213a3b64219a5e702624548
MichalJanikUR <wmicjan86@gmail.com>
2025-01-13 10:56:46 +0100

    Dodanie funkcji wartości bezwzględnej a

--- kalkulator.c
+++ kalkulator.c
@@ -1,7 +1,31 @@
 #include <stdio.h>
-int main() {
 
+double dzielenie(double a, double b)
+{
+    if (b != 0)
+    {
+        return a / b;
+    }
+    else exit(0);
+}
 
-    
-return 0;
-} 
+double bezwzgledna(double a)
+{
+    if(a >= 0)
+    {
+        return a;
+    }
+    else if(a < 0)
+    {
+        return -a;
+    }
+}
+
+int main()
+{
+    printf("%f",bezwzgledna(5));
+    printf("\n%f",bezwzgledna(5));
+    printf("\n%f",dzielenie(3,4));
+    printf("\n%f",dzielenie(3,0));
+    return 0;
+}
