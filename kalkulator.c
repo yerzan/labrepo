a10fbb599340630e25c7b85a2e0a697d099acd10
YevhenMarchak <marchak879@gmail.com>
2025-01-13 11:13:23 +0100

    Radiany na stopni

--- kalkulator.c
+++ kalkulator.c
@@ -6,13 +6,19 @@ double dzielenie(double a, double b)
     {
         return a / b;
     }
-    else exit(0);
+    else
+        exit(0);
+}
+
+double radiany_na_stopnie(double radiany)
+{
+    return radiany * (180.0 / math.pi);
 }
 
 int main()
 {
-    printf("%f",dzielenie(3,4));
-    printf("\n%f",dzielenie(3,0));
-  
+    printf("%f", dzielenie(3, 4));
+    printf("\n%f", dzielenie(3, 0));
+
     return 0;
 }
